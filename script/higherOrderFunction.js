1d1828c49ae353fd978af7cb5128406ec32c6582
ayaavdews <ayaavdews@gmail.com>
2022-03-12 22:40:12 +0700

    pertemuan11 - higher order function (latihan map, filter, reduce)

--- script/higherOrderFunction.js
+++ script/higherOrderFunction.js
@@ -1,132 +1,49 @@
 // Javascript Lanjutan (Advanced Javascript)
-// Higher Order Function (penerapan)
-
-/**
- * 
- * Definisi 
- *    - Function yang beroperasi pada function yang lain. Baik itu digunakan
- *      dalam argument, maupun sebagai return value. (https://eloquentjavascript.net)
- *        
- *        > dalam javascript function disebut First Class Function, artinya inti dari
- *          javascript adalah function. Dan Javascript memperlakukan function sebagai object.
- * 
- * 
- * > function yang disimpan sebagai argumen disebut callback.
- * > function yang memiliki callback sebagai argumen-nya disebut higher order function
- * 
- * 
- * Kenapa menggunakan Higher Order Function
- *    > Abstraksi
- *      membuat kode yang dibuat lebih sederhana. Karena dengan menggunakan function kita bisa
- *      menyembunyikan kerumitan. 
- *    > dengan terbiasa membuat segala sesuatunya kedalam function, kita masuk ke 
- *      sebuah pendekatan yang namanya Functional Programming
- * 
- * 
- * Contoh Higher Order Function yang sering digunakan, terutama  
- * untuk pengelolaan array : 
- *    > Array.prototype.map()
- *    > Array.prototype.filter()
- *    > Array.prototype.reduce()
- * 
- */
-
-const angka = [-1, 8, 9, 1, 4, -5, -4, 3, 2, 9]
-console.log(angka)
-
-/**
- * 
- *  __________________________
- * |    Filter                |
- * |    mencari angka >= 3    |
- * |__________________________|
- * 
- */
-
-// menggunakan for 
-// const newAngka = [];
-// for (let i=0; i<angka.length; i++) {
-//   if (angka[i] >= 3) {
-//     newAngka.push(angka[i])
-//   }
-// }
-
-// const newAngka = angka.filter(function(i){
-//   return i >= 3
-// })
-const newAngka = angka.filter(i => i >= 3)
-console.log(newAngka)
-
+// // Latihan map, filter, radius
 
 
 /**
- * 
- *  ________________________________________________________________________________
- * |    Map                                                                         |
- * |    memetakan tiap-tiap elemen didalam arraynya menggunakan fungsi yang baru    |
- * |    * kalikan semua elemen dalam array angka dengan 2(dua)                      |
- * |________________________________________________________________________________|
- * 
+ * const angka = [-1, 8, 9, 1, 4, -5, -4, 3, 2, 9]
+ *
+ * filter
+ * const newAngka = angka.filter(i => i >= 3)
+ *
+ * map
+ * const mapAngka = angka.map(a => a * 2)
+ *
+ * reduce
+ * const reduceAngka1 = angka.reduce((i, j) => i + j)
+ * const reduceAngka2 = angka.reduce((i, j) => i + j, 4) 
+ *
+ * method chaining
+ * const angkaChaining = angka.filter(i => i > 5).map(i => i * 3).reduce((i,j) => i+j) 
  */
 
-const mapAngka = angka.map(a => a * 2)
-console.log(mapAngka)
 
 
+// ambil semua elemen video
+const videos   = Array.from(document.querySelectorAll('[data-duration]')) 
+// pilih JAVASCRIPT LANJUTAN
+let jsLanjutan = videos.filter(video => video.textContent.includes('JAVASCRIPT LANJUTAN'))
+                  // ambil durasi masing-masing video 
+                  .map(item => item.dataset.duration) 
+                  // ubah durasi menjadi float, ubah menit menjadi detik
+                  .map(waktu => {
+                    const parts = waktu.split(':').map(part => parseFloat(part))
+                    return parts[0]*60 + parts[1]
+                  }) 
+                  // jumlahkan semua detik
+                  .reduce((total, detik) => total + detik) 
 
-/**
- * 
- *  ___________________________________________________________
- * |    Reduce                                                 |
- * |    melakukan sesuatu terhadap seluruh elemen array-nya    |
- * |    * jumlahkan semua isi array-nya                        |
- * |___________________________________________________________|
- * 
- *    > memerlukan 2 argumen : 
- *      1) accumulator
- *         hasil dari prosesnya (dijumlahkan, dikalikan, dll)
- *      2) currentValue
- *         elemen array yang sedang di looping (sekarang sedang looping di elemen yang mana)
- * 
- *    > contoh dijumlahkan : 
- *      0 + (-1) = -1 => (-1) -> currentValue
- *                        -1  -> accumulator
- * 
- *      -1 + 8   = 7  => 8 -> currentValue
- *                       7 -> accumulator
- * 
- *    > secara default ditambahkan 1 argumen diakhir return yang nilainya 0 
- *      -1 + 8 + 9 + 1 + 4 + -5 + -4 + 3 + 2 + 9
- *      0 +-1 + 8 + 9 + 1 + 4 + -5 + -4 + 3 + 2 + 9
- *    
- *    > nilai default bisa kita ganti
- *      default 0    = 0 +-1 + 8 + 9 + 1 + 4 + -5 + -4 + 3 + 2 + 9 = 26
- *      nilai baru 4 = 4 +-1 + 8 + 9 + 1 + 4 + -5 + -4 + 3 + 2 + 9 = 30
- * 
- */
-
-const reduceAngka = angka.reduce((accumulator, currentValue) => accumulator + currentValue)
-const reduceAngkaNonDefault = angka.reduce((accumulator, currentValue) => accumulator + currentValue, 4) 
-console.log(reduceAngka) // 26
-console.log(reduceAngkaNonDefault) // 30
+// ubah formatnya menjadi hh:mm:ss
+const hh = Math.floor(jsLanjutan / 3600)
+const mm = Math.floor((jsLanjutan - hh * 3600) / 60)
+const ss = jsLanjutan - hh * 3600 - mm * 60
 
 
+// simpan di DOM
+const total    = document.querySelector('.jumlah-video')
+const duration = document.querySelector('.total-durasi')
 
-/**
- * 
- *  ________________________________________________________________________________
- * |    Method Chaining                                                             |
- * |    menggabungkan fungsi-fungsi pada higher order function dalam 1x eksekusi    |
- * |________________________________________________________________________________|
- *    
- *    > cari angka > 5
- *    > kalikan 3
- *    > jumlahkan
- * 
- */
-
-const angkaChaining = angka
-                        .filter(i => i > 5)   // 8, 9, 9
-                        .map(i => i * 3)      // 24, 27, 27
-                        .reduce((i,j) => i+j) // 78
-console.log(angkaChaining)
+total.textContent    = `${videos.filter(video => video.textContent.includes('JAVASCRIPT LANJUTAN')).length} Video.`
+duration.textContent = `${hh} Jam, ${mm} Menit, ${ss} Detik.`
